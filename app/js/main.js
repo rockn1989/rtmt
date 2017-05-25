84f6074d9d76a1c435d45233cbcec21c260cbdb5
rockn1989 <gold_100@bk.ru>
2017-05-25 17:55:20 +0300

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,29 @@
 'use strict';
 
-(function() {
+$(function() {
 
+/*** SVG FOR ALL BROWSERS ***/    
+	svg4everybody();
 
-})();
+/*** HEADER SEARCH FORM ***/
+
+$('.contacts-search').on('click', function(e) {
+	e.preventDefault();
+	$('.contacts .uk-grid').fadeOut(350, function() {
+		$('.search-form').fadeIn('350', function() {
+			$('.search-form input').focus();
+		});
+	});
+});
+
+$('.search-form a.uk-modal-close').on('click', function() {
+	$('.search-form').fadeOut('350', function() {
+		$('.contacts .uk-grid').fadeIn('350');
+	});
+});
+
+$('#mobile-form').on('click', function() {
+	$('.mobile-form').fadeToggle('350');
+});
+
+});
