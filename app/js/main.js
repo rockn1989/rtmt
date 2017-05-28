ea5500c33c6d65079a4e15363287fe8bd61a9c31
Artem Ryabkov <gold_100@bk.ru>
2017-05-28 14:52:41 +0300

    main-content

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+$(function() {
+
+/*** SVG FOR ALL BROWSERS ***/    
+	svg4everybody();
+
+/*** HEADER SEARCH FORM ***/
+
+$('.contacts-search').on('click', function(e) {
+	e.preventDefault();
+	$('.contacts .uk-grid').fadeOut(350, function() {
+		$('.search-form').fadeIn('350', function() {
+			$('.search-form input').focus();
+		});
+		$('.search-form').toggleClass('uk-animation-slide-right');
+	});
+});
+
+$('.search-form a.uk-modal-close').on('click', function() {
+	$('.search-form').fadeOut('350', function() {
+		$(this).toggleClass('uk-animation-slide-right');
+		$('.contacts .uk-grid').fadeIn('350');
+	});
+});
+
+$('#mobile-form').on('click', function() {
+	$('.mobile-form').fadeToggle('350');
+});
+
+/*** FOOTER TOGGLE LIST MOBILE ***/
+
+$('[data-role="toggle-list"]').on('click', function() {
+	if($(window).width() <= 767) {
+		$(this)
+			.siblings('[data-role="target-list"]')
+			.slideToggle('350', function() {
+				$(this).toggleClass('hidden').removeAttr('style')
+			});
+	}
+});
+
+});
